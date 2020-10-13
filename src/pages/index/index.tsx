bd397c7e9821dacb0f65b16242af2a8bdcde4b80
Ryan-Willpower <surasak.c.work@gmail.com>
2020-10-13 17:44:29 +0700

    chore: align the button and title

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,15 +1,26 @@
 import React from "react"
 import styled from "@emotion/styled"
 
-import Toggle from '../../components/toggle-switch'
+import Toggle from "../../components/toggle-switch"
+
+const TitleSection = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 20px;
+
+  h1 {
+    margin: 0;
+  }
+`
 
 const Index: React.FC = () => {
   return (
     <div>
-      <div>
+      <TitleSection>
         <h1>Products</h1>
         <Toggle />
-      </div>
+      </TitleSection>
     </div>
   )
 }
